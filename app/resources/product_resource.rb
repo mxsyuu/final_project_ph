bb68520e3ff9cbf2546fb2263ede2d744522bb1d
anonymous anonymous <michelle.syu@kellogg.northwestern.edu>
2022-03-14 05:05:16 +0000

    Generate graphiti resources for products

---
+++ app/resources/product_resource.rb
@@ -0,0 +1,19 @@
+class ProductResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :brand, :string
+  attribute :product_name, :string
+  attribute :ph_level, :float
+  attribute :description, :string
+  attribute :purchase_link, :string
+  attribute :image, :string
+  attribute :creator_id, :integer
+  attribute :product_type_id, :integer
+  attribute :validated_by_company, :boolean
+
+  # Direct associations
+
+  # Indirect associations
+
+end
