c377fd58d6b37d48612e90ce4bb8451f2903077a
anonymous anonymous <michelle.syu@kellogg.northwestern.edu>
2022-03-14 05:05:47 +0000

    Integrate Ransack

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,8 @@ class ProductsController < ApplicationController
 
   # GET /products
   def index
-    @products = Product.page(params[:page]).per(10)
+    @q = Product.ransack(params[:q])
+    @products = @q.result(:distinct => true).includes(:creator, :product_type, :bookmarks).page(params[:page]).per(10)
   end
 
   # GET /products/1
