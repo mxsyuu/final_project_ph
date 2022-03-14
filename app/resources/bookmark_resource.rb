265ba23a05a2b17085b9bf6d9118a7aee9120a7b
anonymous anonymous <michelle.syu@kellogg.northwestern.edu>
2022-03-14 05:05:19 +0000

    Generate graphiti resources for bookmarks

---
+++ app/resources/bookmark_resource.rb
@@ -0,0 +1,13 @@
+class BookmarkResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :product_id, :integer
+  attribute :category_id, :integer
+  attribute :user_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
