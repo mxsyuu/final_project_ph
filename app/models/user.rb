aeac8ced274df7f9a9ddf7d7729c742cc4ffa3e6
anonymous anonymous <michelle.syu@kellogg.northwestern.edu>
2022-03-14 05:04:26 +0000

    Direct association between users and products

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :links,
+             :class_name => "Product",
+             :foreign_key => "creator_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
